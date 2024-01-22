b6dd32d55d508beb575b5c2800969167b0fe5904
Luke <verdant.luke@gmail.com>
2024-01-22 13:26:27 -0500

    was thinking of return early, not console.log

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
   }
-  console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
+
 };
 
 
@@ -10,4 +12,5 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Hello", "Hello");
 assertEqual("Hello", "Hel1o");
+// test with object to see it doesn't work
 assertEqual({"Hello":0}, {"Hello": 0});
