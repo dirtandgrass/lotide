e23140734203f700dd78463598f5af16a5686867
Luke <verdant.luke@gmail.com>
2024-01-22 13:55:07 -0500

    modularize and add head

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
@@ -7,10 +8,14 @@ const assertEqual = function(actual, expected) {
 
 };
 
+const test = function() {
+  assertEqual("Lighthouse Labs", "Bootcamp");
+  assertEqual(1, 1);
+  assertEqual("Hello", "Hello");
+  assertEqual("Hello", "Hel1o");
+  // test with object to see it doesn't work
+  assertEqual({"Hello":0}, {"Hello": 0});
+};
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Hello", "Hello");
-assertEqual("Hello", "Hel1o");
-// test with object to see it doesn't work
-assertEqual({"Hello":0}, {"Hello": 0});
+/* eslint-env node */
+module.exports = assertEqual;
