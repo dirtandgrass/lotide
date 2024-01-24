a814a6fe85234edd54d7f37f9e45e599e2fa2c3e
Luke <verdant.luke@gmail.com>
2024-01-24 13:22:09 -0500

    flatten function impl

---
+++ flatten.js
@@ -0,0 +1,17 @@
+const assertArraysEqual = require("./assertArraysEqual");
+
+const flatten = (array, target = []) => {
+  for (let element of array) {
+    if (Array.isArray(element)) { // need to flatten subarray
+      flatten(element, target); // call flatten with subarray
+    } else {
+      target.push(element);
+    }
+  }
+  return target;
+};
+
+assertArraysEqual(flatten([1,2,3]),[1,2,3]);
+assertArraysEqual(flatten([1,[2,3,[4]]]),[1,2,3,4]);
+assertArraysEqual(
+  flatten([1,2,3,[4,5,6],[7,[8,[9]]]]),[1,2,3,4,5,6,7,8,9]);
