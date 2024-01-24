73992202dba4714420b4e43a0000655aee6630f8
Luke <verdant.luke@gmail.com>
2024-01-23 21:58:57 -0500

    eqArrays implementation: make so all tests say 'pass'

--- eqArrays.js
+++ eqArrays.js
@@ -25,6 +25,6 @@ const eqArrays = function(actual, expected) {
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false);
 assertEqual(eqArrays([1, 2,[ 3, 4]], [1, 2, [ 3, 4]]), true);
-assertEqual(eqArrays([1, 2,[ 3, 4]], [1, 2, [ 4, 5]]), true);
+assertEqual(eqArrays([1, 2,[ 3, 4]], [1, 2, [ 4, 5]]), false);
