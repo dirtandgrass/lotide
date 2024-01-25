7ff4d75c136541b4d148f67e602274e066d42e5b
Luke <verdant.luke@gmail.com>
2024-01-25 12:53:07 -0500

    small updates to tail

--- tail.js
+++ tail.js
@@ -7,9 +7,7 @@ const tail = function(array) {
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Ma", ",","Cellist", "Extraordinaire"];
-
 const theTail = tail(words); // no need to capture the return value since we are not checking it
-
 assertEqual(words.length, 5); // original array should still have 5 elements!
 assertEqual(theTail.length, words.length - 1); // original array should still have 5 elements!
 for (let i = 1; i < words.length; i++) { // check each element of theTail with the original array
@@ -19,9 +17,9 @@ for (let i = 1; i < words.length; i++) { // check each element of theTail with t
 // test empty array
 const emptyArray = [];
 const emptyTail = tail(emptyArray);
-assertEqual(emptyTail.length, 0, 'test empty array');
+assertEqual(emptyTail.length, 0);
 
 // test 1 element array
 const oneElementArray = ['1'];
 const oneElementTail = tail(oneElementArray);
-assertEqual(oneElementTail.length, 0,'test 1 element array');
+assertEqual(oneElementTail.length, 0);
