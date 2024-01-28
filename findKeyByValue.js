fb99d6a01c141db0e46740a3795ede6017a4bac3
Luke <verdant.luke@gmail.com>
2024-01-27 21:04:10 -0500

    findKeyByValue impl

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const assertEqual = require("./assertEqual");
+
+const findKeyByValue = (object, value) => {
+  for (const key in object) { // loop over each key in object
+    if (object[key] === value) { // if key/property value matches value
+      return key;
+    }
+  }
+
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Seinfeld",
+  drama: "Suits",
+  reality: "Below Deck",
+  fantasy: "Lost"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Suits"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Lost"), "fantasy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
