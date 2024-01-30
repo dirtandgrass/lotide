c3c9212a8283605569a17c7640f98a04aac19d17
Luke <verdant.luke@gmail.com>
2024-01-30 12:58:21 -0500

    map.js: init impl

---
+++ map.js
@@ -0,0 +1,21 @@
+
+
+const map = (array, mutator) => {
+
+  // ensure input is valid
+  if (!Array.isArray(array)) throw new Error("First argument must be an array");
+  if (typeof mutator !== 'function') throw new Error("Second argument must be a function");
+
+  const result = []; // new, empty array
+  // loop through each element in the input array
+  for (let el in array) {
+    result.push(mutator(array[el])); // mutate each element and push to new array
+  }
+  return result;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const result = map(words, word => word[0]);
+console.log(result);
