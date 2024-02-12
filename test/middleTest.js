f975b4e35b5d369f55934657c24ff9a8392dfd76
Luke <verdant.luke@gmail.com>
2024-02-12 11:15:52 -0500

    added suggested test to middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -15,6 +15,13 @@ describe("#middle", () => {
     assert.deepEqual(oneElementMiddle, []);
   });
 
+  it("make sure the original array was not altered by the middle function", () => {
+    const words = ["hello", "world", "lighthouse"];
+    const result = middle(words);
+    assert.deepEqual(result, ["world"]);
+    assert.strictEqual(words.length, 3);
+  });
+
   it("should return an empty array if given a two element array", () => {
     const twoElementArray = [1, 2];
     const twoElementMiddle = middle(twoElementArray);
