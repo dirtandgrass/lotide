8d8aa5ddd590fcd6f7711d47743014c08ddaf30f
Luke <verdant.luke@gmail.com>
2024-02-12 10:34:40 -0500

    removed purposeful failed assertion in assertEqualTest

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -6,4 +6,4 @@ assertEqual(1, 1);
 assertEqual("Hello", "Hello");
 assertEqual("Hello", "Hel1o");
 // test with object to see it doesn't work
-assertEqual({"Hello":0}, {"Hello": 0});
+//assertEqual({"Hello":0}, {"Hello": 0});
