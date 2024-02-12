d15474033bf43c740917043133196793d2037f62
Luke <verdant.luke@gmail.com>
2024-02-12 10:41:57 -0500

    seperate middle and tests

--- middle.js
+++ middle.js
@@ -15,9 +15,4 @@ const middle = (source) => {
   }
 };
 
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+module.exports = middle;
